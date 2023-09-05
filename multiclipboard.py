a0b129e35fd7d7daf91305f4f57052939c335ce2
Balint Skonda <balint.skonda@idomsoft.hu>
2023-09-05 09:24:36 +0200

    Solved the rest of the project myself

--- multiclipboard.py
+++ multiclipboard.py
@@ -1,6 +1,7 @@
 import sys
 import clipboard
 import json
+import pprint
 
 
 SAVED_DATA = "clipboard.json"
@@ -17,20 +18,44 @@ def load_data(file_path):
             return data
     except:
         return {}
-
-if len(sys.argv) == 2:
-    command = sys.argv[1]
-    data = load_data(SAVED_DATA)
     
-    if command == "save":
-        key = input("Enter a key: ")
-        data[key] = clipboard.paste()
-        save_data(SAVED_DATA, data)
-    elif command == "load":
-        print("load")
-    elif command == "list":
-        print("list")
+
+def search_for_key(file_path):
+    key = input("What key are you looking for? ")
+    with open(file_path, "r") as f:
+        data = json.load(f)
+        for entry in data:
+            if entry == key.lower():
+                print("The key {} contains the following value: '{}'".format(key, data[entry]))
+                return
+        print("There are no keys matching your input.")
+
+
+def list_data(file_path):
+    with open(file_path, "r") as f:
+        data = json.load(f)
+        print("These is the current state of the database: ")
+        pprint.pprint(data)
+
+
+def main():
+    if len(sys.argv) == 2:
+        command = sys.argv[1]
+        data = load_data(SAVED_DATA)
+        if command == "save":
+            key = input("Enter a key: ")
+            data[key] = clipboard.paste()
+            save_data(SAVED_DATA, data)
+            print("Data saved")
+        elif command == "load":
+            search_for_key(SAVED_DATA)
+        elif command == "list":
+            list_data(SAVED_DATA)
+        else:
+            print("Unknown command")
     else:
-        print("Unknown command")
-else:
-    print("Please print exactly 1 command")
+        print("Please print exactly 1 command")
+
+
+if __name__ == '__main__':
+    main()
